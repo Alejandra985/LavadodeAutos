624b28770d21d326778dcc0d8ff30f3b245c2d2f
Alejandra985 <80970837+Alejandra985@users.noreply.github.com>
2021-04-18 00:39:06 -0500

    Add files via upload

---
+++ Vista.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lavadodeautos;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Vista  extends JFrame{
+     public JButton servicio, funcionario, fecha;
+    private JPanel panelB, panelR, panelX;
+    public JLabel lProduccion, lMensaje;
+    
+    public Vista(){
+        
+        getContentPane().setLayout(new BorderLayout());
+        
+        panelB = new JPanel();
+        panelB.setLayout(new FlowLayout());
+        panelR = new JPanel();
+        panelR.setLayout(new FlowLayout());
+        panelX = new JPanel();
+        panelX.setLayout(new FlowLayout());
+        
+        servicio = new JButton("Por tipo de servicio");
+        funcionario = new JButton("Por funcionario");
+        fecha = new JButton("Por rango de fecha");
+        
+        lProduccion = new JLabel("Produccion diaria:");
+        lMensaje = new JLabel("ES NECESARIO INGRESAR TIPO DE SERVICIO "+"\n"+"PARA PODER CALCULAR CORRECTAMENTE");
+        
+        panelB.add(servicio);
+        panelB.add(funcionario);
+        panelB.add(fecha);
+        
+        panelR.add(lProduccion);
+        
+        panelX.add(lMensaje);
+        
+        add(panelX, BorderLayout.NORTH);
+        add(panelB, BorderLayout.CENTER);
+        add(panelR, BorderLayout.SOUTH);
+    }
+  
+}
